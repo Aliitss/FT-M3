de5aecb7dd491c03461a9f30adfbc3fae0186313
Aliitss <alebolivarparq@gmail.com>
2022-07-05 11:15:17 -0300

    express

--- 05-Express/homework/src/server.js
+++ 05-Express/homework/src/server.js
@@ -1,4 +1,5 @@
 // const bodyParser = require("body-parser");
+const { json } = require("body-parser");
 const express = require("express");
 
 const STATUS_USER_ERROR = 422;
@@ -9,7 +10,128 @@ const posts = [];
 
 const server = express();
 // to enable parsing of json bodies for post requests
-// server.use(express.json());
+server.use(express.json());
+
+server.post('/posts', function(req, res){
+    const {author, title, contents} = req.body;
+
+    if(!author || !title || !contents) res.status(STATUS_USER_ERROR).json(
+        {error: "No se recibieron los parámetros necesarios para crear el Post"})
+
+    
+    let postId = 0;
+    let newPost = {
+        id: postId++,
+        author,
+        title,
+        contents
+    }
+
+    posts.push(newPost);
+    
+    res.json(newPost)
+});
+
+server.post('/posts/author/:author', (req, res) =>{
+    let {title, contents} = req.body
+    let author = req.params.author
+
+    if(!author || !title || !contents) res.status(STATUS_USER_ERROR).json(
+        {error: "No se recibieron los parámetros necesarios para crear el Post"})
+
+        let postId = 0;
+        let newPost = {
+            id: postId++,
+            author,
+            title,
+            contents
+        }
+    
+        posts.push(newPost);
+        
+        res.json(newPost)
+
+
+})
+
+server.get ('/posts', function(req, res) {
+    let  term  = req.query.term
+    
+    if (!term) res.json(posts);
+    let taki = posts.filter(p => p.title.includes(term) || p.contents.includes(term))
+    
+    if (taki.length > 0){
+        res.json(taki)
+    } else {
+        res.json(posts)
+    }
+})
+
+
+server.get ('/posts/:author', (req, res)=>{
+    const { author } = req.params;
+    let filtrados = posts.filter(p => p.author === author);
+
+    if (filtrados.length === 0){
+    return res.status(STATUS_USER_ERROR).json(
+        {error: "No existe ningun post del autor indicado"})
+    }
+    res.json(filtrados)
+})
+
+// Si existen Post que coincidan con ambos parametros, author y title devolver aquellos Posts que correspondan con la información provista, es decir que coincidan author y title.
+
+// Caso contrario, devolver un JSON con un objeto de la forma {error: "No existe ningun post con dicho titulo y autor indicado"}. Verificar que el código de error sea el adecuado.
+
+server.get ('/posts/:author/:title', (req, res)=>{
+    const { author, title } = req.params;
+    let filtrados = posts.filter(p => p.author === author && p.title === title);
+
+    if (filtrados.length === 0){
+    return res.status(STATUS_USER_ERROR).json(
+        {error: "No existe ningun post con dicho titulo y autor indicado"})
+    }
+    res.json(filtrados)  
+
+})
+
+server.put ('/posts', (req, res)=> {
+    const { id, title, contents } = req.body;
+
+    if(!id || !title || !contents){
+        return res.status(STATUS_USER_ERROR).json({error: "No se recibieron los parámetros necesarios para modificar el Post"})
+    }
+
+    const post = posts.find(p => p.id === id)
+
+    if (!post) {
+        return res.status(STATUS_USER_ERROR).json({error: "No hay post con ese id"})    
+    }
+
+    post.title = title;
+    post.contents = contents;
+
+    res.json(post)
+
+});
+
+server.delete ('/posts', (req, res) =>{
+    let { id } = req.body;
+
+    if(!id){
+        return res.status(STATUS_USER_ERROR).json({error: "Mensaje de error"})
+    }
+
+    let foundPost = posts?.find(post => post.id === id)
+
+    if(!foundPost){
+        return res.status(STATUS_USER_ERROR).json({error: "Mensaje de error"})
+    }
+
+    posts = posts.filter(p=> p.id === id)
+    res.json({ success: true })
+
+})
 
 // TODO: your code to handle requests
 
